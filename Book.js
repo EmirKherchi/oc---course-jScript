79aade25120d57988da6be27de7bf96ba278dc2d
emir <contact@ekherchi.fr>
2020-03-19 09:15:52 +0100

    final change

--- Book.js
+++ Book.js
@@ -8,43 +8,47 @@ export class Book {
     this.read = read;
   }
   readbook(page) {
-    if (page >= 1 && page < this.pages) {
+    if(page < 1 || page > this.pages){
+      return 0;
+    }else if(page >= 1 && page < this.pages){
       this.currentPage = page;
       return 1;
-    } else if (page === this.pages) {
+    }else if(page === this.pages){
       this.currentPage = page;
       this.read = true;
       return 1;
-    } else {
-      return 0;
     }
   }
 }
 
-let LePrisonnierAzkaban;
-let sorcerySchool;
-let theDeathReliques;
+const book1 = new Book(
+  "Harry Potter and the azkaban's prisoner",
+  "J.K Rowling",
+  "Harry ans his friends have a new adventure, where harry's godfather wants to kill him after escaping of azkaban, the famous prison",
+  252,
+  250,
+  false,
+);
+const book2 = new Book(
+  "Harry Potter and the sorcery school",
+  "J.K Rowling",
+  "Harry discover hogwart",
+  300,
+  300,
+  true,
+);
+
+const book3 = new Book(
+  "Harry Potter and the azkaban's prisoner",
+  "J.K Rowling",
+  "Harry seeking those three legendary objects",
+  582,
+  582,
+  true,
+);
 
 export const books = [
-  (LePrisonnierAzkaban = new Book(
-    "Harry Potter and the azkaban's prisoner",
-    "J.K Rowling",
-    "Harry ans his friends have a new adventure, where harry's godfather wants to kill him after escaping of azkaban, the famous prison",
-    252,
-    50,
-  )),
-  (sorcerySchool = new Book(
-    "Harry Potter and the sorcery school",
-    "J.K Rowling",
-    "Harry discover hogwart",
-    300,
-    100,
-  )),
-  (theDeathReliques = new Book(
-    "Harry Potter and the azkaban's prisoner",
-    "J.K Rowling",
-    "Harry seeking those three legendary objects",
-    582,
-    1,
-  ))
+ book1,
+ book2,
+ book3,
 ];
